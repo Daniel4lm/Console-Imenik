581097fb0c8acfea0573ff47a9bb5f6943281458
Daniel Molnar <daniel4molnar@gmail.com>
2020-03-27 14:09:52 +0100

    Added function to list all person by first letter or first part of name

--- src/imenik/PhoneBook.java
+++ src/imenik/PhoneBook.java
@@ -17,7 +17,8 @@ public class PhoneBook {
 	private static final String userName = "root";
 	private static final String password = "43OsLeOlRoEtXe43";
 	// localhost//imeBazeNaKojuSeSpajamo
-	private static final String CONN_STRING = "jdbc:mysql://localhost/TelefonskiImenik?useSSL=false&serverTimezone=UTC";
+	private static final String CONN_STRING = "jdbc:mysql://localhost:3306/TelefonskiImenik?allowMultiQueries=true&"
+											+ "useSSL=false&serverTimezone=UTC";
 	// error? dodati na kraj stringa ?useSSL=false&serverTimezone=UTC
 
 	private Connection connDB = null;
@@ -25,13 +26,124 @@ public class PhoneBook {
 	private PreparedStatement preparedStatement;
 	private ResultSet setResults;
 
-	// default constructor
+	// defaultni konstruktor
 	public PhoneBook() {
 		entryList = new ArrayList<Entry>();
 		scanner = new Scanner(System.in);
 		openDBConnection();
 	}
 
+	/* Ispisivanje svih zapisa iz imenika */
+	private void printPersons() {
+
+		String printQuery = "SELECT imenik.ID, osoba.Ime, osoba.Prezime, imenik.Br_tel FROM osoba"
+				+ " INNER JOIN imenik ON imenik.ID_Osoba = osoba.ID_Osoba";
+		try {
+			standardStatement = connDB.createStatement();
+			setResults = standardStatement.executeQuery(printQuery);
+
+			System.out.println("\nTabela: " + setResults.getMetaData().getTableName(1));
+			
+			System.out.printf("%n| %-3s | %-10s | %-15s | %-15s |%n" +
+					          "|------------------------------------------------------|%n", setResults.getMetaData().getColumnName(1),
+							  setResults.getMetaData().getColumnName(2), setResults.getMetaData().getColumnName(3),
+							  setResults.getMetaData().getColumnName(4));
+
+			while (setResults.next()) {
+				System.out.printf("| %-3d | %-10s | %-15s | %-15s |%n", setResults.getInt(1), setResults.getString(2),
+						setResults.getString(3), setResults.getString(4));
+			}
+
+		} catch (SQLException printEx) {
+			System.err.println(printEx);
+		} finally {
+			try {
+				standardStatement.close();
+				setResults.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/* Ispisivanje osoba sa svim telefonima vezanim za istu osobu */
+	private void printPersonByName() {
+
+		System.out.println("Unesite podatke :");
+		System.out.print("Unesite ime >_ ");
+		String fName = scanner.next();
+		System.out.print("Unesite prezime >_ ");
+		String lName = scanner.next();
+
+		String joinQuery = "SELECT Br_tel FROM imenik INNER JOIN osoba ON imenik.ID_Osoba = osoba.ID_Osoba "
+				+ "WHERE osoba.Ime = '" + fName + "' AND osoba.Prezime = '" + lName + "'";
+
+		try {
+
+			if (checkPerson(fName, lName) == 0) {
+				System.out.println("Osoba ne postoji u bazi podataka!");
+				return;
+			}
+
+			standardStatement = connDB.createStatement();
+			setResults = standardStatement.executeQuery(joinQuery);
+
+			System.out.print("\n" + fName + ", " + lName);
+
+			while (setResults.next()) {
+				System.out.print(" : " + setResults.getString(1));
+			}
+			System.out.println();
+
+		} catch (SQLException joinEx) {
+			System.err.println(joinEx);
+		} finally {
+			try {
+				standardStatement.close();
+				setResults.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void printEntriesByLetters() {
+		
+		System.out.println("Unesite jedno ili vise pocetnih slova imena(odvojeno zarezom ili praznim mjestom): ");
+		String[] niz = scanner.nextLine().split("(\\s)|(\\,\\s+)|(\\,)");
+		
+		String letterQuery = ""; 
+		
+		for(String s : niz) {			
+			letterQuery += "SELECT Ime, Prezime, Br_tel FROM imenik INNER JOIN osoba ON imenik.ID_Osoba = osoba.ID_Osoba " + 
+						   "WHERE Ime LIKE '" + s + "%';";
+		}
+				
+		try {
+			preparedStatement = connDB.prepareStatement(letterQuery);
+			preparedStatement.execute();
+			//int i = 0;
+			System.out.println("\nOsobe pronadjene u bazi sa pocetnim slovima :\n" + 
+					   		   "*********************************************");			
+            do {
+                try (ResultSet rs = preparedStatement.getResultSet()) {
+                	  	                	
+                    while (rs.next()) {
+                        System.out.print(rs.getString(1) + ": " + rs.getString(2) + ": " + rs.getString(3));
+                        System.out.println();
+                    }
+                }
+                //i++;
+            } while (preparedStatement.getMoreResults());
+			
+            System.out.println("*********************************************");	
+            
+		} catch (Exception e) {
+			System.err.println(e);
+		}		
+	}
+	
+	/* Provjera da li osoba postoji u tabeli 'osoba' */
 	private Integer checkPerson(String fname, String lname) throws SQLException {
 
 		String checkPersonQuery = "SELECT ID_Osoba FROM osoba WHERE Ime = '" + fname + "' AND Prezime = '" + lname
@@ -51,6 +163,7 @@ public class PhoneBook {
 		return 0;
 	}
 
+	/* Provjera da li vec postoji osoba sa istim brojem u zapisu imenika */
 	private boolean checkPhoneNumber(String fname, String lname, String number) throws SQLException {
 
 		String checkIDQuery = "SELECT ID_Osoba FROM imenik WHERE ID_Osoba = " + checkPerson(fname, lname)
@@ -70,6 +183,7 @@ public class PhoneBook {
 		return false;
 	}
 
+	/* Funkcija za unos novih osoba i telefona u imenik */
 	private void insertNewEntry() {
 
 		System.out.println("Unesite podatke :");
@@ -130,33 +244,6 @@ public class PhoneBook {
 		}
 	}
 
-	private void printPersons() {
-
-		String printQuery = "SELECT imenik.ID, osoba.Ime, osoba.Prezime, imenik.Br_tel FROM osoba"
-				+ " INNER JOIN imenik ON imenik.ID_Osoba = osoba.ID_Osoba";
-		try {
-			standardStatement = connDB.createStatement();
-			setResults = standardStatement.executeQuery(printQuery);
-
-			System.out.printf("%n| %-3s | %-10s | %-15s | %-15s |%n", "ID", "First name", "Last name", "Tel. number");
-
-			while (setResults.next()) {
-				System.out.printf("| %-3d | %-10s | %-15s | %-15s |%n", setResults.getInt(1), setResults.getString(2),
-						setResults.getString(3), setResults.getString(4));
-			}
-
-		} catch (SQLException printEx) {
-			System.err.println(printEx);
-		} finally {
-			try {
-				standardStatement.close();
-				setResults.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
 	/* Funkcija koja vraca max ID iz tabele o studentima */
 	private Integer returnMaxID() throws SQLException  {
 		
@@ -187,7 +274,7 @@ public class PhoneBook {
 		String fName = scanner.next();
 		System.out.print("Unesite prezime >_ ");
 		String lName = scanner.next();
-
+		
 		String deleteQuery = "DELETE FROM imenik WHERE ID_Osoba = " + 
 							 "(SELECT ID_Osoba FROM osoba WHERE Ime = '" + fName + "' AND Prezime = '" + lName + "')";
 
@@ -215,50 +302,9 @@ public class PhoneBook {
 				e.printStackTrace();
 			}
 		}
-
 	}
 
-	private void printPersonByName() {
-
-		System.out.println("Unesite podatke :");
-		System.out.print("Unesite ime >_ ");
-		String fName = scanner.next();
-		System.out.print("Unesite prezime >_ ");
-		String lName = scanner.next();
-
-		String joinQuery = "SELECT Br_tel FROM imenik " + "INNER JOIN osoba ON imenik.ID_Osoba = osoba.ID_Osoba "
-				+ "WHERE osoba.Ime = '" + fName + "' AND osoba.Prezime = '" + lName + "'";
-
-		try {
-
-			if (checkPerson(fName, lName) == 0) {
-				System.out.println("Osoba ne postoji u bazi podataka!");
-				return;
-			}
-
-			standardStatement = connDB.createStatement();
-			setResults = standardStatement.executeQuery(joinQuery);
-
-			System.out.print("\n" + fName + ", " + lName);
-
-			while (setResults.next()) {
-				System.out.print(" : " + setResults.getString(1));
-			}
-			System.out.println();
-
-		} catch (SQLException joinEx) {
-			System.err.println(joinEx);
-		} finally {
-			try {
-				standardStatement.close();
-				setResults.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-	}
-
+	/* Konekcija za bazom podataka */
 	private boolean openDBConnection() {
 
 		try {
@@ -282,12 +328,18 @@ public class PhoneBook {
 	}
 
 	private void printChoice() {
-		System.out.print("\n... Dobrodosli u Web Imenik ...\n\n" + " Izbornik:\n" + " 1 - Dodajte novi unos u imenik\n"
-				+ " 2 - Izlistajte sve unose iz imenika\n" + " 3 - Pronadji broj telefona po imenu\n"
-				+ " 4 - Editujte unos iz imenika\n" + " 5 - Obrišite unos iz imenika\n" + " 0 - Izlaz iz programa\n "
-				+ ">_ ");
+		System.out.print("\n... Dobrodosli u Web Imenik ...\n\n" + 
+						 " Izbornik:\n" + " 1 - Dodajte novi unos u imenik\n" + 
+						 " 2 - Izlistajte sve unose iz imenika\n" + 
+						 " 3 - Pronadji broj telefona po imenu i prezimenu\n" +
+						 " 4 - Pronadji osobe po prvom slovu u imenu\n" + 
+						 " 5 - Editujte unos iz imenika\n" + 
+						 " 6 - Obrišite unos iz imenika\n" + 
+						 " 0 - Izlaz iz programa\n " + 
+						 ">_ ");
 	}
 
+	/* Glavni menij aplikacije */
 	public void appMenu() {
 
 		int choice = 0;
@@ -297,6 +349,7 @@ public class PhoneBook {
 			printChoice();
 
 			choice = scanner.nextInt();
+			scanner.nextLine();
 
 			switch (choice) {
 
@@ -317,12 +370,17 @@ public class PhoneBook {
 
 			case 4:
 
+				printEntriesByLetters();
 				break;
+				
 			case 5:
+				
+				break;
+				
+			case 6:
 				
 				deletePersonEntry();
 				break;
-				
 			case 0:
 
 				closeDBConnection();
